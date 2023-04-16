dd4298e7b1ba841bfcbdb8b910b6dd69153aec63
Malcolm Nixon <Malcolm.nixon@gmail.com>
2023-04-15 21:58:24 -0400

    Initial project setup with basic staging for two scenes.

---
+++ components/helpers/zone_switch_area.gd
@@ -0,0 +1,42 @@
+@tool
+extends Area3D
+
+
+signal triggered
+
+
+## Zone scene file
+@export_file('*.tscn') var zone_scene : String = ""
+
+## If true the zone switcher is enabled
+@export var enabled : bool = true
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+
+# Called when a body enters this area
+func _on_body_entered(body : Node3D):
+	# Ignore if not enabled
+	if not enabled:
+		return
+
+	# Skip if it wasn't the player entering
+	if not body.is_in_group("player_body"):
+		return
+
+	# Find our scene base
+	var scene_base : XRToolsSceneBase = XRTools.find_xr_ancestor(self, "*", "XRToolsSceneBase")
+	if not scene_base:
+		return
+
+	# Report triggering
+	triggered.emit()
+
+	# Fire the load_scene signal
+	if zone_scene == "":
+		scene_base.reset_scene()
+	else:
+		scene_base.load_scene(zone_scene)
---
+++ game/swimming_level/movement/movement_swimming.gd
@@ -0,0 +1,155 @@
+@tool
+class_name XRToolsMovementSwimming
+extends XRToolsMovementProvider
+
+
+## XR Tools Movement Provider for Swimming
+##
+## This script provides swimming movement for the player.
+##
+
+
+## Enumeration of controller to use for swimming
+enum SwimmingController {
+	LEFT,		## Use left controller
+	RIGHT,		## Use right controler
+}
+
+## Enumeration of pitch control input
+enum SwimmingPitch {
+	HEAD,		## Head controls pitch
+	CONTROLLER,	## Controller controls pitch
+}
+
+## Enumeration of bearing control input
+enum SwimmingBearing {
+	HEAD,		## Head controls bearing
+	CONTROLLER,	## Controller controls bearing
+	BODY,		## Body controls bearing
+}
+
+
+## Movement provider order
+@export var order : int = 30
+
+## Flight controller
+@export var controller : SwimmingController = SwimmingController.LEFT
+
+## Flight pitch control
+@export var pitch : SwimmingPitch = SwimmingPitch.CONTROLLER
+
+## Flight bearing control
+@export var bearing : SwimmingBearing = SwimmingBearing.CONTROLLER
+
+## Swimming speed from control
+@export var speed_scale : float = 3.0
+
+## Swimming traction pulling flight velocity towards the controlled speed
+@export var speed_traction : float = 3.0
+
+## Swimming drag
+@export var drag : float = 0.1
+
+## Guidance effect (virtual fins/wings)
+@export var guidance : float = 0.1
+
+## Swimming controller
+var _controller : XRController3D
+
+
+# Node references
+@onready var _camera := XRHelpers.get_xr_camera(self)
+@onready var _left_controller := XRHelpers.get_left_controller(self)
+@onready var _right_controller := XRHelpers.get_right_controller(self)
+
+
+# Add support for is_xr_class on XRTools classes
+func is_xr_class(name : String) -> bool:
+	return name == "XRToolsMovementSwimming" or super(name)
+
+
+func _ready():
+	# In Godot 4 we must now manually call our super class ready function
+	super()
+
+	# Get the flight controller
+	if controller == SwimmingController.LEFT:
+		_controller = _left_controller
+	else:
+		_controller = _right_controller
+
+
+# Process physics movement for flight
+func physics_movement(delta: float, player_body: XRToolsPlayerBody, disabled: bool):
+	# Disable flying if requested, or if no controller
+	if disabled or !enabled or !_controller.get_is_active():
+		return
+
+	# Select the pitch vector
+	var pitch_vector: Vector3
+	if pitch == SwimmingPitch.HEAD:
+		# Use the vertical part of the 'head' forwards vector
+		pitch_vector = -_camera.transform.basis.z.y * player_body.up_player_vector
+	else:
+		# Use the vertical part of the 'controller' forwards vector
+		pitch_vector = -_controller.transform.basis.z.y * player_body.up_player_vector
+
+	# Select the bearing vector
+	var bearing_vector: Vector3
+	if bearing == SwimmingBearing.HEAD:
+		# Use the horizontal part of the 'head' forwards vector
+		bearing_vector = -player_body.up_player_plane.project(
+				_camera.global_transform.basis.z)
+	elif bearing == SwimmingBearing.CONTROLLER:
+		# Use the horizontal part of the 'controller' forwards vector
+		bearing_vector = -player_body.up_player_plane.project(
+				_controller.global_transform.basis.z)
+	else:
+		# Use the horizontal part of the 'body' forwards vector
+		var left := _left_controller.global_transform.origin
+		var right := _right_controller.global_transform.origin
+		var left_to_right := right - left
+		bearing_vector = player_body.up_player_plane.project(
+				left_to_right.rotated(player_body.up_player_vector, PI/2))
+
+	# Construct the flight bearing
+	var forwards := (bearing_vector.normalized() + pitch_vector).normalized()
+	var side := forwards.cross(player_body.up_player_vector)
+
+	# Construct the target velocity
+	var joy_forwards := _controller.get_vector2("primary").y
+	var joy_side := _controller.get_vector2("primary").x
+	var heading := forwards * joy_forwards + side * joy_side
+
+	# Calculate the flight velocity
+	var flight_velocity := player_body.velocity
+	flight_velocity *= 1.0 - drag * delta
+	flight_velocity = flight_velocity.lerp(heading * speed_scale, speed_traction * delta)
+
+	# Apply virtual guidance effect
+	if guidance > 0.0:
+		var velocity_forwards := forwards * flight_velocity.length()
+		flight_velocity = flight_velocity.lerp(velocity_forwards, guidance * delta)
+
+	# If exclusive then perform the exclusive move-and-slide
+	player_body.velocity = player_body.move_body(flight_velocity)
+	return true
+
+# This method verifies the movement provider has a valid configuration.
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings := super()
+
+	# Verify the camera
+	if !XRHelpers.get_xr_camera(self):
+		warnings.append("Unable to find XRCamera3D")
+
+	# Verify the left controller
+	if !XRHelpers.get_left_controller(self):
+		warnings.append("Unable to find left XRController3D node")
+
+	# Verify the right controller
+	if !XRHelpers.get_right_controller(self):
+		warnings.append("Unable to find left XRController3D node")
+
+	# Return warnings
+	return warnings
