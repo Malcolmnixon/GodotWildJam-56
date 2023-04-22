494385280238db60434b4ea9965e871f5fe3cc79
stumpynub <joshuawoolman15@gmail.com>
2023-04-22 10:21:25 -0500

    Other shop items added

--- components/ui/ui_shop.gd
+++ components/ui/ui_shop.gd
@@ -1,6 +1,15 @@
 extends Control
 
 
+func _ready():
+	if GameManager.get_shop_item("harpoon"): 
+			$ColorRect/VBoxContainer/HarpoonContainer/HarpoonButton.disabled = true
+	if GameManager.get_shop_item("headlamp"): 
+			$ColorRect/VBoxContainer/HeadlampContainer/HeadlampButton.disabled = true
+	if GameManager.get_shop_item("knife"): 
+		$ColorRect/VBoxContainer/KnifeContainer/KnifeButton.disabled = true
+		
+
 func _process(delta):
 	$ColorRect/Control/CreditsLabel.text = "CREDITS: " + str(get_coins())
 
@@ -13,10 +22,22 @@ func get_coins() -> int:
 	
 	return current_coins
 
+func buy_item(item: String, price): 
+	GameManager.buy_shop_item(item, price)
+	GameManager.emit_signal("item_bought", item)
 
 func _on_harpoon_button_pressed():
-		print("pressed")
 		if get_coins() >= 3: 
-			GameManager.buy_shop_item("harpoon", 3)
-			GameManager.emit_signal("item_bought", "harpoon")
+			buy_item("harpoon", 5)
 			$ColorRect/VBoxContainer/HarpoonContainer/HarpoonButton.disabled = true
+
+func _on_knifelamp_button_button_down():
+	if get_coins() >= 1: 
+		buy_item("knife", 2)
+		$ColorRect/VBoxContainer/KnifeContainer/KnifeButton.disabled = true
+
+func _on_headlamp_button_pressed():
+	if get_coins() >= 2: 
+		buy_item("headlamp", 3)
+		$ColorRect/VBoxContainer/HeadlampContainer/HeadlampButton.disabled = true
+
